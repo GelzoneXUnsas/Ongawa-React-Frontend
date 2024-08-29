b8c5be05bc5bf83159548a49a256944f4462b890
Tylermhh <tyler.hlaing@gmail.com>
2024-08-29 15:09:03 -0700

    hd music covers and click to focus and zoom in art and covers

--- src/GalleryGrid.js
+++ src/GalleryGrid.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./pages/GalleryPage/GalleryPage.module.css"; 
 
 //This is what is used rn. hardcoded. API endpoint doesnt get used currently. its called in the galleryPage files but not sure what happens after...
@@ -11,57 +11,62 @@ import art6 from "../src/assets/images/galleryArt/art6.png";
 import art7 from "../src/assets/images/galleryArt/art7.png";
 import art8 from "../src/assets/images/galleryArt/art8.png";
 
-import music1 from "../src/assets/images/musicCovers/celestialechoes.png";
-import music2 from "../src/assets/images/musicCovers/celticwhispersballad.png";
-import music3 from "../src/assets/images/musicCovers/neonpulsesym.png";
-import music4 from "../src/assets/images/musicCovers/nocturnalpursuit.png";
+import music1 from "../src/assets/images/musicCovers/celestialechoesHD.png";
+import music2 from "../src/assets/images/musicCovers/celticwhispersballadHD.png";
+import music3 from "../src/assets/images/musicCovers/neonpulsesymHD.png";
+import music4 from "../src/assets/images/musicCovers/nocturnalpursuitHD.png";
 
 const artImages = [art1, art2, art3, art4, art5, art6, art7, art8];
 const musicCovers = [music1, music2, music3, music4];
 
 function GalleryGrid(props) {
-    // buggy implementation of modal view
-    // function f(id) { // 100
-    //     document.getElementById(id).style.zIndex = 100;
-    // }
-    // function g(id) { // -100
-    //     document.getElementById(id).style.zIndex = -100;
-    // }
-    const rows = props.galleryData.map((image, index) => { 
-    return (<>
-        {/* <div onClick={() => g(index)} id={index} style={{zIndex:-100, position: 'absolute', left: 0, top: 0, width: "100%", height: "auto"}}>
-        <img src={artImages[image.id-1]} alt={image.description} />
-        </div> */}
-        {/* <div key={index} onClick={() => f(index)}> */}
-        <div key={index}>
-            {/* <img className={styles.artImage} src={artImages[image.id-1]} alt={image.description} /> */}
+    const [selectedImage, setSelectedImage] = useState(null); // State to track the selected image
 
-            {props.currentPage === "art" ? 
-                <img className={styles.artImage} src={artImages[image.id-1]} alt={image.description} /> 
-                :
-                <div className={styles.coverImageAndText}>
-                    <img className={styles.coverImage} src={musicCovers[image.id-1]} alt={image.description} /> 
-                    <div>
-                        {image.name}
+    const handleImageClick = (image) => {
+        setSelectedImage(image); // Set the selected image
+    };
+
+    const closeOverlay = () => {
+        setSelectedImage(null); // Close the overlay by resetting the selected image
+    };
+
+    const rows = props.galleryData.map((image, index) => { 
+        return (
+            <div key={index} onClick={() => handleImageClick(image)}>
+                {props.currentPage === "art" ? 
+                    <img className="artImage object-cover w-48 h-40 flex-shrink-0 rounded-xl shadow-md lg:w-full lg:h-auto" src={artImages[image.id-1]} alt={image.description} /> 
+                    :
+                    <div className="coverImageAndText flex flex-col items-center font-title-lexend text-font-size-xs lg:text-body-overpass-base">
+                        <img className="coverImage object-cover w-72 flex-shrink-0 rounded-xl bg-none m-0 p-0" src={musicCovers[image.id-1]} alt={image.description} /> 
+                        <div>
+                            {image.name}
+                        </div>
                     </div>
-                </div>
-            }
-        </div>
-        </>
-    );
-    }
-    );
+                }
+            </div>
+        );
+    });
+
     return (
         <>
-        {props.currentPage === "art" ? 
-            <div className={styles.imageGridArt}>
-                {rows}
-            </div>
-        :
-            <div className={styles.imageGridArt}>
-                {rows}
-            </div>
-        }
+            {props.currentPage === "art" ? 
+                <div className={styles.imageGridArt}>
+                    {rows}
+                </div>
+            :
+                <div className={styles.imageGridArt}>
+                    {rows}
+                </div>
+            }
+            {/* Overlay */}
+            {selectedImage && (
+                <div className="overlay fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50" onClick={closeOverlay}>
+                    <div className="popupContent flex w-100% p-4 h-auto lg:w-3/5 justify-center">
+                        {/* <button className="closeButton absolute top-4 right-4 text-white text-2xl bg-transparent border-none hover:border-none" onClick={closeOverlay}>×</button> */}
+                        <img className="popupImage object-cover h-auto max-h-screen rounded-xl" src={props.currentPage === "art" ? artImages[selectedImage.id-1] : musicCovers[selectedImage.id-1]} alt={selectedImage.description} />
+                    </div>
+                </div>
+            )}
         </>
     );
 }
