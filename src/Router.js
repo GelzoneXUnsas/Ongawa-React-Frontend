3079d5b86e07f5607d297a46a852b7b2ed4cd230
Tylermhh <tyler.hlaing@gmail.com>
2024-09-05 19:59:32 -0700

    redirect to coming soon, fixed coming soon page

--- src/Router.js
+++ src/Router.js
@@ -30,7 +30,7 @@ function Router() {
                     <Route path="musicianlisting" element={<MusicanListingPage />} />
                     <Route path="musician" element={<MusicianPage />} />
                     <Route path="beatmap" element={<BeatmapPage />} />
-                    <Route path="community" element={<CommunityPage />} />
+                    <Route path="community" element={<ComingSoonPage />} />
                     <Route path="comingsoon" element={<ComingSoonPage />} />
                     <Route path="login" element={<Login />} />
                     <Route path='register' element={<Register />} />
--- src/pages/ComingSoonPage/ComingSoonPage.js
+++ src/pages/ComingSoonPage/ComingSoonPage.js
@@ -3,21 +3,27 @@ import React from "react";
 import styles from "./ComingSoonPage.module.css";
 
 
-import headerBackground from '../../assets/images/headerBackground.png';
+import headerBackgroundImg from '../../assets/images/headerBackground.png';
 
 
 const ComingSoonPage = () => { 
     return (
+        <>
+        
         <div className={styles.comingsoonPage}>
-            <div className={styles.titleContainer}>
-                <img src={headerBackground} className={styles.headerBackground} alt="" />
-                <div className={styles.titleText}>COMING SOON</div>
+            <div className="titleContainer relative h-60 z-0 overflow-hidden lg:h-72">
+                <div className="bgImgContainer w-full lg:-mt-64">
+                    <img src={headerBackgroundImg} className="headerBackgroundImg w-full relative object-cover" alt="" />
+                </div>
+                <div className="absolute w-full h-12 bottom-0 z-3 flex justify-center text-white text-center font-title-lexend text-3xl font-bold">COMING SOON</div>
+                <div className="gradientOverlay absolute bottom-0 w-full h-[70%] bg-gradient-overlay z-1"></div>
             </div>
             <div className={styles.comingsoonContent}>
                 This page is under construction. Thanks for your patience!
             </div>
 
         </div>
+        </>
     );
 };
 
