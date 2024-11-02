8f342226fa2a146b3e4bcdcea24e9658403db0b7
Thien An Tran <fxthienan@gmail.com>
2024-11-01 18:14:41 -0700

    chore: update configs to use ES module syntax & add zip files as assets in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.zip'], // include .zip files (treat .zip files as an asset rather than a module)
 })
