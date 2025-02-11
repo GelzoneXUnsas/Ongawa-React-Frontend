aae9473549a9061fdd44387812d725f44ac52147
Tyler Baxter <tybaxter@calpoly.edu>
2025-02-11 10:03:28 -0800

    setup db

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,11 +1,32 @@
-import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
+import { a, defineData, type ClientSchema } from "@aws-amplify/backend";
 
 const schema = a.schema({
-  Todo: a.model({
-      content: a.string(),
-      isDone: a.boolean()
-    })
-    .authorization(allow => [allow.publicApiKey()])
+    Todo: a
+        .model({
+            content: a.string(),
+            isDone: a.boolean(),
+        })
+        .authorization((allow) => [allow.publicApiKey()]),
+    Posts: a
+        .model({
+            post_id: a.string(),
+            user_id: a.string(),
+            header: a.string(),
+            content: a.string(),
+            like_count: a.integer(),
+            reply_count: a.integer(),
+            timestamp: a.string(),
+        })
+        .authorization((allow) => [allow.publicApiKey()]),
+    Interactions: a
+        .model({
+            post_id: a.string(),
+            user_id: a.string(),
+            type: a.enum(["like", "reply", "subreply"]),
+            timestamp: a.string(),
+            content: a.string(),
+        })
+        .authorization((allow) => [allow.publicApiKey()]),
 });
 
 // Used for code completion / highlighting when making requests from frontend
@@ -13,9 +34,9 @@ export type Schema = ClientSchema<typeof schema>;
 
 // defines the data resource to be deployed
 export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: 'apiKey',
-    apiKeyAuthorizationMode: { expiresInDays: 30 }
-  }
+    schema,
+    authorizationModes: {
+        defaultAuthorizationMode: "apiKey",
+        apiKeyAuthorizationMode: { expiresInDays: 30 },
+    },
 });
--- src/index.jsx
+++ src/index.jsx
@@ -8,17 +8,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./global.css";
-import './index.css';
+import "./index.css";
 import Router from "./Router";
 import { BrowserRouter } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Amplify } from 'aws-amplify';
-import { record } from 'aws-amplify/analytics';
-import outputs from '../amplify_outputs.json';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Amplify } from "aws-amplify";
+import { record } from "aws-amplify/analytics";
+import outputs from "../amplify_outputs.json";
 
 Amplify.configure({
-  ...Amplify.getConfig(),
-  Analytics: amplifyconfig.Analytics,
+    ...Amplify.getConfig(),
+    //   Analytics: amplifyconfig.Analytics,
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -28,5 +28,4 @@ root.render(
             <Router />
         </BrowserRouter>
     </React.StrictMode>
-
 );
