fc1fc81242547e4fa69831557876e1aaf6f960af
agent773 <parth773@berkeley.edu>
2025-10-28 18:57:54 -0700

    Fixed backend

--- src/index.jsx
+++ src/index.jsx
@@ -14,11 +14,10 @@ import Router from "./Router";
 import { BrowserRouter } from "react-router-dom";
 import { Amplify } from "aws-amplify";
 //import { record } from 'aws-amplify/analytics';
-//import outputs from '../amplify_outputs.json';
+import outputs from '../amplify_outputs.json';
 
-Amplify.configure({
-  ...Amplify.getConfig(),
-});
+Amplify.configure(outputs);
+// eslint-disable-next-line no-undef
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
